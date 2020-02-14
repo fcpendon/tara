e198e6843a8da5a08d6cff8be43ed8b834582c84
Ferdie Pendon <fcpendon@gmail.com>
2020-02-15 05:33:49 +0800

    Added routes for API and vue component.

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,9 @@ use Illuminate\Http\Request;
 
 Route::middleware('api')->group(function () {
     Route::get('/countries', 'CountryController@all');
+    Route::get('/{country}', 'CountryController@info');
     Route::get('/{country}/cities', 'CountryController@cities');
     Route::get('/{country}/{city}', 'CityController@info');
+    Route::get('/{country}/{city}/forecast', 'CityController@forecast');
+    Route::any('/{country}/{city}/search', 'CityController@search');
 });
